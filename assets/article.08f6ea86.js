cb9dffbfbacc82ca8efc5202051c55774ddf23ad
yqs2022 <1814926166@qq.com>
2022-11-29 10:51:25 +0800

    511

--- assets/article.08f6ea86.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./index.5e675851.js";const n=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/info/get",e),r=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/content/list",e);export{r as a,n as g};
