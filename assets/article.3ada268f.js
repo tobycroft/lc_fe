74250ffec08de3d2cbf63f5b8d266a18acb36306
yqs2022 <1814926166@qq.com>
2022-12-08 14:47:09 +0800

    526

---
+++ assets/article.3ada268f.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index.584ba2b0.js";const n=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/info/get",e),r=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/content/list",e);export{r as a,n as g};
