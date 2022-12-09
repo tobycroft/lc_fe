09e2a27da014475c0a233806f225c6f7f6f45e3d
yqs2022 <1814926166@qq.com>
2022-12-09 16:00:37 +0800

    577

---
+++ assets/article.3b24bc3d.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index.a9c0de4d.js";const n=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/info/get",e),r=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/content/list",e);export{r as a,n as g};
