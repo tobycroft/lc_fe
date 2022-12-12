c2c0539befa47804e20ddf3d9436bb7aa027b117
yqs2022 <1814926166@qq.com>
2022-12-12 16:34:48 +0800

    584

---
+++ assets/article.4725d713.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index.f20618ab.js";const n=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/info/get",e),r=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/content/list",e);export{r as a,n as g};
