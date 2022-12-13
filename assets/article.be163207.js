772bbf7967183b8619af1c50516c4d8a0c558a85
yqs2022 <1814926166@qq.com>
2022-12-13 18:00:55 +0800

    533

---
+++ assets/article.be163207.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index.6cadc58e.js";const n=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/info/get",e),r=async e=>await o("https://api.lc.familyeducation.org.cn/v1/article/content/list",e);export{r as a,n as g};
