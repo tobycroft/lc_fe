5f7317a044f112849b32f59906b8d9c70b25bacb
yqs2022 <1814926166@qq.com>
2023-02-08 14:55:35 +0800

    证书，领取奖品修改

--- assets/Card.ec9bc618.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as t,c as s,N as o}from"./index.8507dc5f.js";const n={name:"Card"},_={class:"card-container"};function d(e,r,c,p,f,l){return t(),s("div",_,[o(e.$slots,"default",{},void 0,!0)])}const u=a(n,[["render",d],["__scopeId","data-v-dbf5a290"]]);export{u as default};
